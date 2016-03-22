eaf4c6c12a536bd571ed7d6d93b8d83aac1a0415
AndjelaBabic <andjelasbabic@gmail.com>
2016-03-23 00:44:38 +0100

    Napravljene klase Valuta i Kurs, ubacene get i set metode

---
+++ Menjacnica/src/valute/Valuta.java
@@ -0,0 +1,34 @@
+package valute;
+
+import java.util.LinkedList;
+
+import kursevi.Kurs;
+
+public class Valuta {
+	private String naziv, skraceniNaziv;
+	private LinkedList<Kurs> kursevi;
+
+	public String getNaziv() {
+		return naziv;
+	}
+
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+
+	public String getSkraceniNaziv() {
+		return skraceniNaziv;
+	}
+
+	public void setSkraceniNaziv(String skraceniNaziv) {
+		this.skraceniNaziv = skraceniNaziv;
+	}
+
+	public LinkedList<Kurs> getKursevi() {
+		return kursevi;
+	}
+
+	public void setKursevi(LinkedList<Kurs> kursevi) {
+		this.kursevi = kursevi;
+	}
+}
