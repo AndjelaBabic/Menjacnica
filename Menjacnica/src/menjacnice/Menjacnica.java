4d6ed760242c35886dae3699ffafb5c81799baa9
AndjelaBabic <andjelasbabic@gmail.com>
2016-03-23 01:16:27 +0100

    Implementirane metode u klasi Menjacnica

--- Menjacnica/src/menjacnice/Menjacnica.java
+++ Menjacnica/src/menjacnice/Menjacnica.java
@@ -1,28 +1,64 @@
 package menjacnice;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import interfejsi.MenjacnicaInterfejs;
 import kursevi.Kurs;
 import valute.Valuta;
 
 public class Menjacnica implements MenjacnicaInterfejs {
-
+	LinkedList<Valuta> valute;
 	@Override
 	public void dodajKurs(Valuta valuta, GregorianCalendar datum, double prodajni, double srednji, double kupovni) {
 		// TODO Auto-generated method stub
-
+		Kurs k = new Kurs();
+		k.setKupovni(kupovni);
+		k.setProdajni(prodajni);
+		k.getSrednji();
+		k.setDatum(datum);
+		for (int i = 0; i < valute.size(); i++) {
+			if(valute.get(i)!=null){
+				if(valute.get(i).equals(valuta)){
+					valute.get(i).getKursevi().add(k);
+					break;
+				}
+			}
+		}
 	}
 
 	@Override
 	public void brisiKurs(Valuta valuta, GregorianCalendar datum) {
 		// TODO Auto-generated method stub
-
+		for (int i = 0; i < valute.size(); i++) {
+			if(valute.get(i)!=null){
+					if(valute.get(i).equals(valuta)){
+						for (int j = 0; j < valute.get(i).getKursevi().size(); j++) {
+							Kurs k = valute.get(i).getKursevi().get(j);
+							if(k.getDatum().equals(datum)){
+								valute.get(i).getKursevi().remove(j);
+							}
+						}
+					}
+				}
+		}
 	}
 
 	@Override
 	public Kurs pronadjiKurs(Valuta valuta, GregorianCalendar datum) {
 		// TODO Auto-generated method stub
+		for (int i = 0; i < valute.size(); i++) {
+			if(valute.get(i)!=null){
+					if(valute.get(i).equals(valuta)){
+						for (int j = 0; j < valute.get(i).getKursevi().size(); j++) {
+							Kurs k = valute.get(i).getKursevi().get(j);
+							if(k.getDatum().equals(datum)){
+								return k;
+							}
+						}
+					}
+				}
+		}
 		return null;
 	}
 
